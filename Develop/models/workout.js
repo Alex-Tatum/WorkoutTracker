021035de6902c0106fcde1e86885fa3fad520c91
Alex Tatum <tatum_alex@ymail.com>
2021-02-23 11:17:39 -0500

    added dependencies

---
+++ Develop/models/workout.js
@@ -0,0 +1,48 @@
+// Required Dependencies
+const mongoose = require("mongoose");
+
+// Creating Mongoose schema
+const Schema = mongoose.Schema;
+
+// Creating WorkoutSchema
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        default: 0,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
