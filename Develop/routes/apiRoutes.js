1f593b34f8bf972648ec523f729c0ff167616735
Alex Tatum <tatum_alex@ymail.com>
2021-02-23 11:04:16 -0500

    added api and html routes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,83 @@
+// Required Dependencies
+const router = require("express").Router();
+const db = require("../models");
+
+// router.get("/api/workouts", (req, res) => {
+//   db.Workout.find({})
+//     .then((dbWorkout) => {
+//       res.json(dbWorkout);
+//     })
+//     .catch((err) => {
+//       res.json(err);
+//     });
+// });
+//  GET route that retrieves workouts from the database
+router.get("/api/workouts", (req, res) => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// POST route that adds a new workout to the database
+router.post("/api/workouts", async (req, res) => {
+  db.Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// PUT route that updates an existing workout in the database
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: {
+        exercises: body,
+      },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// GET route that gets a range of workouts from the database
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.aggregate([
+    { $sort: { day: -1 } },
+    { $limit: 7 },
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
